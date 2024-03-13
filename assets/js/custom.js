36e413b1949c1cb77f5b67a0be2d5873827030a6
Gaurav Kumar <frontendsoni@gmail.com>
2024-03-13 19:06:29 +0530

    adding functionality for updating task

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,13 +4,10 @@ const inputBtn = document.querySelector('.add_btn');
 const taskList = document.querySelector('.task_list');
 
 
-
-const currentDate = new Date();
-const date = currentDate.getDate();
-const month = currentDate.getMonth();
-const year = currentDate.getFullYear();
 //function for date
 const updatedDate = () => {
+
+  const currentDate = new Date();
 
   const dayOptions = {weekday:'long'};
   const weekDay = currentDate.toLocaleDateString('en-US',dayOptions);
@@ -18,8 +15,8 @@ const updatedDate = () => {
   const MonthOptions = {month:'long'};
   const monthName = currentDate.toLocaleDateString('en-US',MonthOptions);
 
-  dateBox.innerHTML = `<span class="day">${weekDay}, ${date} </span> 
-                      <span class="month">${monthName}, ${year} </span>` ;
+  dateBox.innerHTML = `<span class="day">${weekDay}, ${currentDate.getDate()} </span> 
+                      <span class="month">${monthName}, ${currentDate.getFullYear()} </span>` ;
 
 }
 
@@ -27,23 +24,79 @@ updatedDate();
 
 //function for adding task
 
+const addTask = (taskText, date, month, year) => {
+
+  const newTask = document.createElement('li');
+    newTask.innerHTML = `
+                            <div class="todo_content">
+                              <span class="checkbox"></span>
+                              <p>${taskText}</p>
+                            </div>
+                            <div class="update_content">
+                              <span class="task_date">${date}/${month+1}/${year}</span>
+                              <i class="fa-solid fa-trash-can del_task_btn"></i>
+                              <i class="fa-solid fa-pencil edit_task_btn"></i>
+                            </div>
+                        `
+    taskList.appendChild(newTask);
+}
+
 inputBtn.addEventListener('click', () => {
 
-  const taskText = inputValue.value;
-  const newTask = document.createElement('li');
-
-  newTask.innerHTML = `
-                          <div class="todo_content">
-                            <span class="checkbox"></span>
-                            <p>${taskText}</p>
-                          </div>
-                          <div class="update_content">
-                            <span class="task_date">${date}/${month+1}/${year}</span>
-                            <i class="fa-solid fa-trash-can"></i>
-                            <i class="fa-solid fa-pencil"></i>
-                          </div>
-                      `
-  taskList.appendChild(newTask);
-  inputValue.value = '';                    
+  const taskText = inputValue.value.trim();
+  if (taskText) {
+    const taskCurrentDate = new Date();
+    addTask(taskText, taskCurrentDate.getDate(), taskCurrentDate.getMonth(), taskCurrentDate.getFullYear())
+    inputValue.value = '';
+  }
 })
+
+//Task all functionality
+
+taskList.addEventListener('click', (event) => {
+  //delete
+  if (event.target.classList.contains('del_task_btn')) {
+    event.target.closest('li').remove();
+  };
+  //complete(done)
+  if (event.target.classList.contains('checkbox')) {
+    const taskBox = event.target.closest('.todo_content');
+    const taskContent = taskBox.querySelector('p');
+    taskContent.classList.toggle('completed');
+    event.target.classList.toggle('tick');
+  }
+  //edit task text
+  if (event.target.classList.contains('edit_task_btn')) {
+
+    const targetListItem = event.target.closest('li');
+    const taskContent = targetListItem.querySelector('p');
+    const inputElem = document.createElement('input');
+    inputElem.type = 'text';
+    inputElem.className = 'edit_task_text';
+    inputElem.value = taskContent.textContent;
+    taskContent.replaceWith(inputElem);
+
+    const deleteIcon = event.target;
+    const doneIcon = document.createElement('i')
+    doneIcon.className = 'fa-regular fa-square-check edit_done_btn';
+    deleteIcon.replaceWith(doneIcon);
+
+  }
+
+  if (event.target.classList.contains('edit_done_btn')) {
+
+    const targetListItem = event.target.closest('li');
+    const taskInput = targetListItem.querySelector('input');
+    const newText = taskInput.value
+    const taskParagraph = document.createElement('p');
+    taskParagraph.textContent = newText;
+    taskInput.replaceWith(taskParagraph);
+
+    const editDoneIcon = event.target;
+    const deleteIcon = document.createElement('i')
+    deleteIcon.className = 'fa-solid fa-pencil edit_task_btn';
+    editDoneIcon.replaceWith(deleteIcon);
+  }
+
+});
 
